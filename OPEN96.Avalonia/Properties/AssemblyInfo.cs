78239371554c395c25798b2378ad0c85f162cf55
root <root>
2025-03-21 22:25:07 +0800

    增加程序集 XmlnsDefinition XmlnsPrefix

---
+++ OPEN96.Avalonia/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using Avalonia.Metadata;
+
+[assembly: XmlnsDefinition("https://github.com/OPEN96/OPEN96.Avalonia", "OPEN96.Avalonia")]
+[assembly: XmlnsDefinition("https://github.com/OPEN96/OPEN96.Avalonia", "OPEN96.Avalonia.Controls")]
+[assembly: XmlnsDefinition("https://github.com/OPEN96/OPEN96.Avalonia", "OPEN96.Avalonia.DataTemplates")]
+[assembly: XmlnsDefinition("https://github.com/OPEN96/OPEN96.Avalonia", "OPEN96.Avalonia.Helpers")]
+[assembly: XmlnsDefinition("https://github.com/OPEN96/OPEN96.Avalonia", "OPEN96.Avalonia.ViewModels")]
+
+[assembly: XmlnsPrefix("https://github.com/OPEN96/OPEN96.Avalonia", "open96")]
